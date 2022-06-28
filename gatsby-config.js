3056114cc8bbe359a82df2751812dcbe618f9f95
Nicolas Hitosis <nicolashitosis@Nicolass-MacBook-Pro.local>
2022-06-27 18:46:57 -0700

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  siteMetadata: {
+    title: `What would you like to call your site?✔ · My First Gatsby Site`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
